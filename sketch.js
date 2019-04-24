ec65cd9f2f54d7ca09d668f7cf5068361e92eec9
Beaudan <beau@loki.network>
2019-04-24 13:39:49 +1000

    Split some stuff up

--- sketch.js
+++ sketch.js
@@ -1,9 +1,6 @@
 /*jshint esversion: 8 */
-// const fetch = require('node-fetch');
 
 let swarms = {};
-// const serviceNodes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-const testSnodes = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 const swarmRadius = 80;
 const snodeRadius = 10;
 const propCol = {r: 106, g: 55, b: 255};
@@ -24,86 +21,80 @@ const snodeCols = {
   [stateEnum.snodeMessage]: propCol,
 };
 
-let protect = 0;
+const createSwarm = (swarmId) => {
+  let x, y, r;
+  while(true) {
+    let overlapping = false;
+    x = random(swarmRadius, width - swarmRadius);
+    y = random(swarmRadius, height - swarmRadius);
+    r = swarmRadius;
 
-const createSwarms = async () => {
-  const params = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ method: 'get_service_nodes' }),
-  }
-  const response = await fetch(lokidUrl, params);
-  let serviceNodes;
-  if (!response.headers.get('Content-Type') === 'application/json') {
-    return;
+    Object.keys(swarms).forEach(swarmId => {
+      const other = swarms[swarmId];
+      const d = dist(x, y, other.x, other.y);
+      if (d < r + other.r + 6) {
+        overlapping = true;
+      }
+    });
+    if (!overlapping) {
+      break;
+    }
   }
 
-  responseJson = await response.json();
-  const snodeList = JSON.parse(responseJson.result.as_json);
+  let swarm = {
+    x,
+    y,
+    r,
+    snodes: []
+  }
+  swarms[swarmId] = swarm;
+}
+
+const createSnode = (swarm, pubkey) => {
+  const swarmX = swarm.x;
+  const swarmY = swarm.y;
+  const snodes = swarm.snodes;
+  let x, y, r;
+  while(true) {
+    let overlapping = false;
+    const a = random() * 2 * PI;
+    r = swarmRadius * Math.sqrt(random()) - snodeRadius * 2;
+    x = r * Math.cos(a) + swarmX;
+    y = r * Math.sin(a) + swarmY;
+
+    Object.keys(snodes).forEach(pubkey => {
+      const other = snodes[pubkey];
+      const d = dist(x, y, other.x, other.y);
+      if (d < snodeRadius + snodeRadius + 6) {
+        overlapping = true;
+      }
+    });
+    if (!overlapping) {
+      break;
+    }
+  }
+  const snode = {
+    x,
+    y,
+    r: snodeRadius,
+    state: stateEnum.default,
+  }
+  swarm.snodes[pubkey] = snode;
+}
+
+const init = async () => {
+  const response = await httpPost(lokidUrl, 'json', { method: 'get_service_nodes' })
+
+  const snodeList = JSON.parse(response.result.as_json);
   snodeList.forEach(snodeData => {
     const pubkey = snodeData.pubkey;
     const swarmId = snodeData.info.swarm_id;
     if (!Object.keys(swarms).includes(swarmId.toString())) {
-      let x, y, r;
-      while(true && protect < 1000) {
-        let overlapping = false;
-        protect += 1;
-        x = random(swarmRadius, width - swarmRadius);
-        y = random(swarmRadius, height - swarmRadius);
-        r = swarmRadius;
-
-        Object.keys(swarms).forEach(swarmId => {
-          const other = swarms[swarmId];
-          const d = dist(x, y, other.x, other.y);
-          if (d < r + other.r + 6) {
-            overlapping = true;
-          }
-        });
-        if (!overlapping) {
-          break;
-        }
-      }
-
-      let swarm = {
-        x,
-        y,
-        r,
-        snodes: []
-      }
-      swarms[swarmId] = swarm;
+      createSwarm(swarmId);
     }
-    const swarmX = swarms[swarmId].x;
-    const swarmY = swarms[swarmId].y;
-    const snodes = swarms[swarmId].snodes;
-    let x, y, r;
-    while(true && protect < 1000) {
-      let overlapping = false;
-      protect += 1;
-      const a = random() * 2 * PI;
-      r = swarmRadius * Math.sqrt(random()) - snodeRadius * 2;
-      x = r * Math.cos(a) + swarmX;
-      y = r * Math.sin(a) + swarmY;
-
-      Object.keys(swarms[swarmId].snodes).forEach(pubkey => {
-        const other = swarms[swarmId].snodes[pubkey];
-        const d = dist(x, y, other.x, other.y);
-        if (d < snodeRadius + snodeRadius + 6) {
-          overlapping = true;
-        }
-      });
-      if (!overlapping) {
-        break;
-      }
+    if (!Object.keys(swarms[swarmId]).includes(pubkey)) {
+      createSnode(swarms[swarmId], pubkey);
     }
-    const snode = {
-      x,
-      y,
-      r: snodeRadius,
-      state: stateEnum.default,
-    }
-    swarms[swarmId].snodes[pubkey] = snode;
   });
 }
 
@@ -121,14 +112,13 @@ const updateSnodes = () => {
 }
 
 var setup = async () => {
+  init();
   createCanvas(1000, 800);
   frameRate(1);
-  await createSwarms();
 }
 
 var draw = () => {
   clear()
-  console.log('Frame');
   // Draw all the swarms
   Object.keys(swarms).forEach(swarmId => {
     const swarm = swarms[swarmId];
